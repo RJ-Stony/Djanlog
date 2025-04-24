09144aed916340cd2c30aa0c10ec56a343c2acea
노준석 <joshua052698@gmail.com>
2025-04-24 11:26:47 +0900

    add: implement landing and about me views, update URL routing, and create corresponding HTML templates

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+def landing(request):
+    return render(request, 'pages/landing.html')
+
+def about_me(request):
+    return render(request, 'pages/about_me.html')
