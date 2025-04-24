df3e20b25a66972d2085da390fb9ff71dab3a44e
노준석 <joshua052698@gmail.com>
2025-04-24 11:32:01 +0900

    refactor: update view functions to class-based views and adjust URL routing; add post detail template

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,11 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from .models import Post
 
-def index(request):
-    posts = Post.objects.all().order_by('-pk')
-    context = {'posts': posts}
-    return render(request, 'blog/index.html', context)
+class PostList(ListView):
+    model = Post
+    ordering = '-pk'
+    template_name = 'blog/index.html'
 
-def post_page(request, pk):
-    post = Post.objects.get(pk=pk)
-    context = {'post': post}
-    return render(request, 'blog/post_page.html', context)
+class PostDetail(DetailView):
+    model = Post
+    
