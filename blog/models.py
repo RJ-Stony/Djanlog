c41a7a320bd5259fcfd9b872481479cbda131915
노준석 <joshua052698@gmail.com>
2025-04-24 12:37:05 +0900

    feat: add hook field to Post model and display in post templates

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,7 @@ import os
 
 class Post(models.Model):
     title = models.CharField(max_length=50)
+    hook = models.CharField(max_length=100, blank=True)
     content = models.TextField()
     thumbnail = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True)
     upload = models.FileField(upload_to='blog/files/%Y/%m/%d', blank=True)
