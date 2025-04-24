2f31601a08c1a2cd3fbc1d67c9c6397abb3aba68
노준석 <joshua052698@gmail.com>
2025-04-24 22:19:19 +0900

    add: landing page styles with background image and card design

--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from blog.models import Post
 
 def landing(request):
-    return render(request, 'pages/landing.html')
+    recent_posts = Post.objects.order_by('-pk')[:3]
+    context = {
+        'recent_posts': recent_posts,
+    }
+    return render(request, 'pages/landing.html', context)
 
 def about_me(request):
     return render(request, 'pages/about_me.html')
