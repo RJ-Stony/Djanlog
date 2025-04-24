6985b404d00dae01c29faf9f46d882e3c9f20044
노준석 <joshua052698@gmail.com>
2025-04-24 12:11:53 +0900

    feat: add upload field to Post model for file handling

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,7 @@ class Post(models.Model):
     title = models.CharField(max_length=50)
     content = models.TextField()
     thumbnail = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True)
+    upload = models.FileField(upload_to='blog/files/%Y/%m/%d', blank=True)
     created = models.DateTimeField(auto_now=True)
     updated = models.DateTimeField(auto_now=True)
     
