f67174192aba8298c904d1e9fa2e59eeb19eb6aa
노준석 <joshua052698@gmail.com>
2025-04-24 17:18:22 +0900

    feat: add comment deletion and update functionality with modal confirmation

--- blog/views.py
+++ blog/views.py
@@ -120,7 +120,17 @@ class PostUpdate(LoginRequiredMixin, UpdateView):
                 self.object.tags.add(tag)
             
         return response
-
+    
+class CommentUpdate(LoginRequiredMixin, UpdateView):
+    model = Comment
+    form_class = CommentForm
+    
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated and request.user == self.get_object().author:
+            return super(CommentUpdate, self).dispatch(request, *args, **kwargs)
+        else:
+            raise PermissionDenied
+        
 def new_comment(request, pk):
     if request.user.is_authenticated:
         post = get_object_or_404(Post, pk=pk)
@@ -138,12 +148,12 @@ def new_comment(request, pk):
     else:
         raise PermissionDenied
     
-class CommentUpdate(LoginRequiredMixin, UpdateView):
-    model = Comment
-    form_class = CommentForm
+def delete_comment(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    post = comment.post
     
-    def dispatch(self, request, *args, **kwargs):
-        if request.user.is_authenticated and request.user == self.get_object().author:
-            return super(CommentUpdate, self).dispatch(request, *args, **kwargs)
-        else:
-            raise PermissionDenied
+    if request.user.is_authenticated and request.user == comment.author:
+        comment.delete()
+        return redirect(post.get_absolute_url())
+    else:
+        raise PermissionDenied
