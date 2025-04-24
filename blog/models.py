2b4d41383f8923c3210b6f2e5c5ff982eacd1b4c
노준석 <joshua052698@gmail.com>
2025-04-24 11:20:51 +0900

    add: implement post detail view and update index template for post links

--- blog/models.py
+++ blog/models.py
@@ -8,3 +8,7 @@ class Post(models.Model):
 
     def __str__(self):
         return f'{self.title}'
+    
+    def get_absolute_url(self):
+        return f'/blog/{self.pk}/'
+    
--- blog/views.py
+++ blog/views.py
@@ -5,3 +5,8 @@ def index(request):
     posts = Post.objects.all().order_by('-pk')
     context = {'posts': posts}
     return render(request, 'blog/index.html', context)
+
+def post_page(request, pk):
+    post = Post.objects.get(pk=pk)
+    context = {'post': post}
+    return render(request, 'blog/post_page.html', context)
