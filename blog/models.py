a65360d5364dea93dcad116715f1d0446489b871
노준석 <joshua052698@gmail.com>
2025-04-24 11:05:04 +0900

    update: register Post model in admin and adjust timezone settings

--- blog/models.py
+++ blog/models.py
@@ -3,4 +3,8 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=50)
     content = models.TextField()
-    created = models.DateTimeField()
+    created = models.DateTimeField(auto_now=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.title}'
