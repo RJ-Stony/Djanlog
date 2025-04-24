599dc0d5ff57670e1cf7bc53273ef3360fcd19f3
노준석 <joshua052698@gmail.com>
2025-04-24 12:09:53 +0900

    feat: add media handling for blog post thumbnails and update templates

--- blog/models.py
+++ blog/models.py
@@ -3,9 +3,10 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=50)
     content = models.TextField()
+    thumbnail = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True)
     created = models.DateTimeField(auto_now=True)
     updated = models.DateTimeField(auto_now=True)
-
+    
     def __str__(self):
         return f'{self.title}'
     
